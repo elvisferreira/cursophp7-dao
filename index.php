27974920ec9f621624e76643c90dbac957560e7e
elvisferreira <elvisferreira@hotmail.com>
2017-07-10 20:51:05 -0300

    listagem

--- index.php
+++ index.php
@@ -9,10 +9,25 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
+/*
 $root = new Usuario();
 
 $root->loadbyID(3);
 
 echo $root;
+*/
+
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);*/
+
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+
+echo $usuario;
 
  ?>
