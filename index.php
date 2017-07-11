0086565ae61e423070510d6fab5b1616a2893722
elvisferreira <elvisferreira@hotmail.com>
2017-07-10 21:27:46 -0300

    comando delete

--- index.php
+++ index.php
@@ -4,48 +4,42 @@ require_once("config.php");
 
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);*/
 
 /*
 $root = new Usuario();
-
 $root->loadbyID(3);
-
 echo $root;
 */
 
 
 /*$lista = Usuario::getList();
-
 echo json_encode($lista);*/
 
 
 /*$search = Usuario::search("jo");
-
 echo json_encode($search);*/
 
 
 /*$usuario = new Usuario();
 $usuario->login("user", "12345");
-
 echo $usuario;*/
 
 
 /*$aluno = new Usuario("aluno", "@alun0");
-
 $aluno->insert();
-
 echo $aluno;*/
 
-$usuario = new Usuario();
 
+/*$usuario = new Usuario();
 $usuario->loadById(9);
-
 $usuario->update("professor", "!@#$%¨&*");
+echo $usuario;*/
 
+$usuario = new Usuario();
+$usuario->loadByID(9);
+$usuario->delete();
 echo $usuario;
 
  ?>
